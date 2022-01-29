7db96360bd5b4f49e3ed068e80173f9f7c6ab9cd
John Mook <jedwardmook@yahoo.com>
2022-01-28 19:47:37 -0600

    commit tree 24b1048ed9b1484e8704d1771157267d37a830a4

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+function backwardsWord(word) {
+  const wordBreakUp = word.split("");
+  const backwardsSplit = wordBreakUp.reverse();
+  const reverseWord = backwardsSplit.join("");
+  return reverseWord
+};
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reverseWord = backwardsWord(word)
+    if( word === reverseWord){
+      return true;
+    }else {
+      return false;
+    }
+  }
 
 /* 
   Add your pseudocode here
@@ -9,6 +22,10 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+// my thought process is that the function would need to compare
+// first and last letter and then second letter to second to last
+// letter. This got me googling ways to break down strings into pieces
+// my thought process from that is to use charAt
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
